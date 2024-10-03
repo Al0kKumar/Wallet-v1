4b11ee49fe819a89c9ae893237aeda360b62787b
Alok <mishraalok189381@gmail.com>
2024-10-03 09:29:47 +0530

    updated the server listening port

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,7 @@ app.use('/api/users',userRoutes);
 app.use('/api/accounts',accountRoutes)
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 
 app.listen(PORT, () => {
