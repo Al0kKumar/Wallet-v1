178b77b05a0b6f711697679658a3ee065d18aff8
Alok <mishraalok189381@gmail.com>
2024-10-04 07:15:05 +0530

    done with it

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -5,7 +5,7 @@ function Button({label}) {
     <div className='flex justify-center'>
         <button
         className='text-white text-center
-        bg-blue-500 px-12 py-3 ml-5 rounded-md mt-5'
+        bg-blue-500 px-12 py-3 ml-5  rounded-md mt-2'
         >
           {label}
         </button>
